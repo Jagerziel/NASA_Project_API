0c72bcfc981b8fd2cdeb728a4f9a9bb14b8a6df5
jagerziel <fromnytosg@gmail.com>
2022-12-19 08:50:27 -0500

    data-downloads successful

--- db/NASA/downloadProjectData.js
+++ db/NASA/downloadProjectData.js
@@ -4,15 +4,21 @@ import fs from 'fs';
 import express from "express";
 import dotenv from 'dotenv'
 dotenv.config()
+import dataProjId from './data-ProjectID.json' assert {type: 'json'}
 
-//Fetching and writing data from API
-for (let i = 0; i < 1; i++) {
-    let id = '116443'
-    fetch(`${process.env.API_URL20}${id}${process.env.API_URL21}${process.env.API_KEY}`)
+//Iterate through ID to pull only subset of NASA Projects in second Fetch
+const idArray = []
+for (let i = 0; i < dataProjId.length; i++) {
+    idArray.push(dataProjId[i].projectId)
+}
+
+// Fetching and writing data from API
+for (let i = 0; i < idArray.length; i++) {
+    fetch(`${process.env.API_URL20}${idArray[i]}${process.env.API_URL21}${process.env.API_KEY}`)
     .then(response => response.json() )
     .then(data => {
-        console.log(data)
-        // fs.writeFileSync('./db/NASA/data-ProjectData.json', JSON.stringify(data))
+        fs.writeFileSync('./db/NASA/data-ProjectData.json', JSON.stringify(data))
     })
 }
+
 
