aa562c5c81b9b94890ee3fd5019835ba7c8e8205
jagerziel <fromnytosg@gmail.com>
2022-12-20 12:35:57 -0500

    dynamic menu on api root page

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,26 @@
 import { Router } from "express";
 import projectRoutes from "./projects.js";
 
+
+//TESTING
+import data from '../db/NASA/data-ProjectData.json' assert { type: "json" }
+console.log(data.length)
+
 //Variable for instance of Express
 const router = Router();
 
 //Setup API Root
 router.get("/", (req, res) => {
-  res.send("API Root");
+  //TEST CODE
+  res.setHeader('Content-type','text/html')
+  res.write("<h1>API ROOT: Project Directory</h1>");
+
+  for (let i = 0; i < data.length; i++) {
+    res.write(`<p><a href='/api/projects/${data[i].projectId}'>${i + 1}) ${data[i].title} (ID: ${data[i].projectId})</a></p>`)
+  }
+  res.end()
+  //FINAL CODE
+  // res.send("API Root");
 });
 
 //Setup Router Link to ProjectRoutes
